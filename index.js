58d9ae29357c5598aba7763670188ce18b7c362e
Aravind <aravind08222@gmail.com>
2020-10-04 12:16:53 +0530

    changes

--- index.js
+++ index.js
@@ -25,3 +25,49 @@ a.forEach(function (element) {
         document.querySelector(".navbar").classList.toggle("show");
     })
 });
+
+//Function to check email
+
+function validateMail() {   
+    var minput = document.querySelector(".email-input");     
+    var x = minput.value;
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        alert("The entered Email address is not valid");
+    } else {
+        alert("It is a valid email address");
+    }
+}
+
+//Function to check phone number
+
+function validatePhone(){
+    var val = document.querySelector(".number-input");
+    var x = val.value;
+    if(x.length < 10){
+        alert("Enter a valid number");
+    }
+}
+
+//Functio to check age
+
+function validateAge(){
+    var val = document.querySelector(".age-input");
+    var x = val.value;
+    if(x < 150){
+        alert("The enterd age is valid");
+    } else {
+        alert("The age is not valid ");
+    }
+}
+
+//Function to check  name
+
+function validateName(){
+    var val = document.querySelector(".name-input");
+    var x = val.value;
+    if(x == " "){
+        alert("Enter a valid name")
+    }
+}
